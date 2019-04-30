6007c671c3546c4162c08664d17ba5a9bf736eef
Jace Yang <35613800+jacethemagician@users.noreply.github.com>
2019-04-30 11:49:40 -0400

    Update PhoneNumber.java

--- PhoneBook/PhoneNumber.java
+++ PhoneBook/PhoneNumber.java
@@ -1,10 +1,4 @@
-//Jace Yang
-//magician@brandeis.edu
-//March 27, 2019
-//PA#2
-/* Explanation of the program/class */
-//PhoneNumber class provided by instructor
-/* Known Bugs: none */
+//Jace Yang, jaceyang@brandeis.edu
 
 public class PhoneNumber{
     private long number;
